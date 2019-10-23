0abeaaa7ed36e9829db9b1cc6b2ca184503ba6c2
Adri <adrian.olmo@cesjuanpablosegundo.es>
2019-10-23 09:55:37 +0200

    Proyectos Acceso a Datos

---
+++ Proyectos/Proyecto_TicketMaster/src/proyecto_ticketmaster/Localidad.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_ticketmaster;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Adri
+ */
+public class Localidad implements Serializable{
+    int id_localidad;
+    String nombreCiudad;
+
+    public Localidad(int id_localidad, String nombreCiudad) {
+        this.id_localidad = id_localidad;
+        this.nombreCiudad = nombreCiudad;
+    }
+
+    public int getId_localidad() {
+        return id_localidad;
+    }
+
+    public void setId_localidad(int id_localidad) {
+        this.id_localidad = id_localidad;
+    }
+
+    public String getNombreCiudad() {
+        return nombreCiudad;
+    }
+
+    public void setNombreCiudad(String nombreCiudad) {
+        this.nombreCiudad = nombreCiudad;
+    }
+
+    @Override
+    public String toString() {
+        return "Localidad{" + "id_localidad=" + id_localidad + ", nombreCiudad=" + nombreCiudad + '}';
+    }
+    
+}
+
