98f5e7a26cae0b8a12975053036afe8afea4a406
Adri <adrian.olmo@cesjuanpablosegundo.es>
2019-11-08 10:05:48 +0100

    Manejo de ficheros Java

---
+++ Manejo_ficheros/Lectura/src/lectura/Lectura.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lectura;
+
+import java.io.*;
+
+/**
+ *
+ * @author Usuario DAM 2
+ */
+public class Lectura {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException{
+        //Declarar fichero 
+        File fichero = new File("C:\\Users\\Usuario DAM 2\\Documents\\NetBeansProjects\\Acceso_Datos\\Manejo_ficheros\\Lectura\\prueba.txt");
+        //Crear el flujo de entrada
+        FileReader fic = new FileReader(fichero);
+        int i;
+        while ((i = fic.read()) != -1) //se va leyendo un caracter
+        {
+            System.out.print((char) i);
+        }
+        fic.close();// cerrar fichero
+
+    }
+}
